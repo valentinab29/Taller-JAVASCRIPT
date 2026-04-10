c50b064bc730ad28aef5d8613df83bffd103e67e
valentinab29 <dvbolanos@unbosque.edu.co>
2026-04-10 08:41:18 -0500

    Condiciones tablas de verdad

--- TallerVariable.js
+++ TallerVariable.js
@@ -15,3 +15,19 @@ const riesgoBajo = 1.044 ;
 const riesgoMedio = 2.436 ;
 const riesgoAlto = 4.350 ;
 const riesgoMaximo = 6.960 ;
+const porcentajeIBC = 0.7 ;
+const porcentajeSalud = 0.04 ;
+const porcentajePension = 0.04 ;
+let salarioTotal = salario + comisiones + horasExtra ;
+let IBC = salarioTotal * porcentajeIBC ;
+let salud = IBC * porcentajeSalud ;
+let pension = porcentajePension * IBC ;  
+if ( edad < 18 ) { 
+  console.log ("No se puede calcular");
+} else if ( edad >= 18 && edad < 25 ) {
+    console.log ("Usuario beneficiario por cotizante");
+} else if ( edad >= 25 && edad < 60 ) {
+    console.log ("Se calcularà la pensiòn");
+} else { 
+    console.log ("Podrà continuar con el siguiente paso del proceso");
+}
