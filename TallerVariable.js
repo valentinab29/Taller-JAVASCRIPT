5d2ba68306cbef657adefe0a728df2b0cdec8af3
valentinab29 <dvbolanos@unbosque.edu.co>
2026-04-17 08:06:55 -0500

    ARL

--- TallerVariable.js
+++ TallerVariable.js
@@ -2,26 +2,7 @@ let nombreCompleto = "" ;
 let edad = 0 ;
 let tipoDocumento = "" ;
 let numeroDocumento = "" ;
-let salario = 0 ;
-let comisiones = 0 ;
-let horasExtra = 0 ;
-let nivelRiesgo = "" ;
-const salarioMinimoLegal = 1750905 ;
-const salarioMinimoIntegral = 22761765 ;
-const subsidioTransporte = 249095 ;
-const valorTributario = 52.37 ;
-const riesgoMinimo = 0.522 ;
-const riesgoBajo = 1.044 ;
-const riesgoMedio = 2.436 ;
-const riesgoAlto = 4.350 ;
-const riesgoMaximo = 6.960 ;
-const porcentajeIBC = 0.7 ;
-const porcentajeSalud = 0.04 ;
-const porcentajePension = 0.04 ;
-let salarioTotal = salario + comisiones + horasExtra ;
-let IBC = salarioTotal * porcentajeIBC ;
-let salud = IBC * porcentajeSalud ;
-let pension = porcentajePension * IBC ;  
+
 if ( edad < 18 ) { 
   console.log ("No se puede calcular");
 } else if ( edad >= 18 && edad < 25 ) {
@@ -31,3 +12,30 @@ if ( edad < 18 ) {
 } else { 
     console.log ("Podrà continuar con el siguiente paso del proceso");
 }
+
+
+let salario = 0 ;
+let comisiones = 0 ;
+let horasExtra = 0 ;
+let nivelRiesgo = "" ;
+
+const salarioMinimoLegal = 1750905 ;
+const salarioMinimoIntegral = 22761765 ;
+const subsidioTransporte = 249095 ;
+const valorTributario = 52.37 ;
+const porcentajeIBC = 0.7 ;
+const porcentajeSalud = 0.04 ;
+const porcentajePension = 0.04 ;
+const riesgo = [0.522, 1.044, 2.436, 4.350, 6.960]
+
+let salarioTotal = salario + comisiones + horasExtra ;
+let IBC = calcularPorcentaje(SalarioTotal, porcentajeIBC);
+let salud = calcularPorcentaje(IBC, porcentajeSalud);
+let pension = calcularPorcentaje(IBC, porcentajePension); 
+
+function calcularPorcentaje (base, porcentaje) {
+  return base * porcentaje;
+}
+
+let fondoSolidaridad = calcularPorcentaje(IBC, porcentFondoSolidaridad);
+IBC>=4*SMLV ? fondoSolidaridad : fondoSolidaridad = 0;
